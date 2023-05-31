9487ca4967e6f4e0122a64682005bd6d3ff4afc7
Sanchit Poudel <snchtpdl12@gmail.com>
2023-05-31 23:54:12 +0545

    bundesliga main css

--- src/Components/Leagues/Bundesliga.jsx
+++ src/Components/Leagues/Bundesliga.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../Main/main.css'
+import '../Main/Main.css'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import TeamRow from '../Main/TeamRow.jsx'
